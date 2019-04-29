207624fbc5e37aca716c33922a65656909b6a1ec
sadia mushtaq <mushtaq_sadia@hotmail.com>
2019-04-29 09:31:22 +0500

    First commit

---
+++ HRMVCCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HRMVCCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
